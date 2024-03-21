585fbf7ed054da7a54ff09bd9707380f8aeebf5c
vas <vasile.vio200@gmail.com>
2024-03-21 22:33:00 +0200

    Modified Board class

--- Board.py
+++ Board.py
@@ -3,19 +3,27 @@ import pygame
 
 class Board:
 
-
-    def __init__(self, width: int, height: int,rows: int, columns: int
-                , board_color: tuple, line_color: tuple):
-        self.width = width
-        self.height = height
-        self.rows = rows
-        self.columns = columns
+    def __init__(self):
         
+        # Number of rown n columns
+        self.rows = 3
+        self.columns = 3
 
+        # Style of the game board
+        self.board_color = (255, 255, 255)
+        self.line_color = (0, 0, 0)
 
+        # Dimension of the board
+        self.width = 600
+        self.height = 600
+        self.screen = pygame.display.set_mode((self.width, self.height))
+
+        # Window title
+        pygame.display.set_caption("TicTacToe Game")
+
+        
     def display_board(self):
-        pygame.init()
-        screen = pygame.display.set_mode((self.width, self.height))
+        pass
 
 
     def check_winner(sedlf):
