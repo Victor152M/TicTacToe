a3ef1e3d3014c882b35294b84ad3027f6f563fab
Victor S <venedaegallia@tutanota.com>
2024-03-21 22:54:56 +0200

    Actually runs

--- Game.py
+++ Game.py
@@ -1,10 +1,11 @@
 from Board import Board
 from Player import Player
+import time
 
 class Game:
     def __init__(self, board: Board, players: list):
         self.board = board
-        self.players
+        self.players = players
         self.current_player = self.players[0]
 
     # Initialize the game by creating the board
@@ -14,17 +15,19 @@ class Game:
     def switch_player(self):
         if self.current_player == self.players[0]:
             self.current_player = self.players[1]
-        elif self.current_player == self.player[1]:
-            self.current_player = self.player[0]
+        elif self.current_player == self.players[1]:
+            self.current_player = self.players[0]
 
     # Start the game loop
     def play_game(self):
         while (True):
             self.current_player.choose_move()
-            if(is_game_over() or is_board_full):
+            if(self.is_game_over() or self.board.is_board_full()):
                 break
+            # questionabel sleep()
+            time.sleep(1)
             self.board.display_board()
-            switch_player()
+            self.switch_player()
 
     def is_game_over(self) -> bool:
         self.board.check_winner()
