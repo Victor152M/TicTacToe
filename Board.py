ca68a705ba6172f3e9f2b12a57ccad2a89c3fd78
vas <vasile.vio200@gmail.com>
2024-03-21 22:08:06 +0200

    Modified Board class

--- Board.py
+++ Board.py
@@ -5,17 +5,23 @@ class Board:
 
 
     def __init__(self, width: int, height: int,rows: int, columns: int
-                 , board_color: tuple, line_color: tuple):
+                , board_color: tuple, line_color: tuple):
         self.width = width
         self.height = height
+        self.rows = rows
+        self.columns = columns
+        
 
 
     def display_board(self):
         pygame.init()
+        screen = pygame.display.set_mode((self.width, self.height))
 
 
+    def check_winner(sedlf):
+        pass
 
 
-
-
-    
+    def is_board_full(self):
+        pass
+   
