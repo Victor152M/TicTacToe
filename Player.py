e49a78e4f55676c0831ff906c1bcb00ca11cb0b0
Victor S <venedaegallia@tutanota.com>
2024-03-22 22:50:43 +0200

    bye

--- Player.py
+++ Player.py
@@ -8,10 +8,6 @@ class Player():
     def capture_move(self):
         print(f"{self.name}'s turn")
         move = input("Enter a number from 1 to 9: ")
-        [
-            ["","",""],
-            ["","",""],
-            ["","",""]
-        ]
+        
 
         
